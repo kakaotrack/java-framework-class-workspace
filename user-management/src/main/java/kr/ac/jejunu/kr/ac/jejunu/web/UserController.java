a66357433031629292a5c8934b94c6bd5f267380
허윤호 <hyh0408@gmail.com>
2016-02-16 18:55:58 +0900

    spring boot init

---
+++ user-management/src/main/java/kr/ac/jejunu/kr/ac/jejunu/web/UserController.java
@@ -0,0 +1,15 @@
+package kr.ac.jejunu.kr.ac.jejunu.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/")
+    String home() {
+        return "forward:list";
+    }
+}
