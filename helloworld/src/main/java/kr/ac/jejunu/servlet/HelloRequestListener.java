c8bf1948d5cc39814f0d826e80ae288ed663a079
허윤호 <hyh0408@gmail.com>
2016-04-28 20:10:09 +0900

    Hello Request Listener

---
+++ helloworld/src/main/java/kr/ac/jejunu/servlet/HelloRequestListener.java
@@ -0,0 +1,27 @@
+package kr.ac.jejunu.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by hyh0408 on 2016. 4. 28..
+ */
+public class HelloRequestListener implements ServletRequestListener{
+
+    private final static Logger logger = LoggerFactory.getLogger(HelloRequestListener.class);
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent sre) {
+        logger.info("******** request listener destroy **********");
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent sre) {
+        logger.info("******** request listener init **********");
+
+    }
+}
