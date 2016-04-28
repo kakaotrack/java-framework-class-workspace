b508e17da90bd8bc1bfc368b243c6a5f0a1274dc
허윤호 <hyh0408@gmail.com>
2016-04-28 19:40:11 +0900

    Hello Context Listener

---
+++ helloworld/src/main/java/kr/ac/jejunu/servlet/HelloContextListener.java
@@ -0,0 +1,25 @@
+package kr.ac.jejunu.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by hyh0408 on 2016. 4. 28..
+ */
+public class HelloContextListener implements ServletContextListener{
+
+    private final static Logger logger = LoggerFactory.getLogger(HelloContextListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("*******  context init ***********");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("*******  context destroy ***********");
+    }
+}
