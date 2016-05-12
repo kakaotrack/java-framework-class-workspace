b5460f59f60ca4ed171590b4a7485cc8687b7056
허윤호 <hyh0408@gmail.com>
2016-05-12 15:21:52 +0900

    SimpleController

---
+++ helloworld/src/main/java/kr/ac/jejunu/spring/SimpleController.java
@@ -0,0 +1,17 @@
+package kr.ac.jejunu.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller("/simple")
+public class SimpleController implements org.springframework.web.servlet.mvc.Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView("simple");
+        modelAndView.addObject("abc", "안녕 세상아");
+        return modelAndView;
+    }
+}
