80ae0730bf723d97f8d48d20161e1b5a4856eb07
허윤호 <hyh0408@gmail.com>
2016-05-19 17:43:27 +0900

    hibernate_get

---
+++ helloworld/src/main/java/kr/ac/jejunu/model/User.java
@@ -0,0 +1,37 @@
+package kr.ac.jejunu.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by hyh0408 on 2016. 5. 19..
+ */
+public class User implements Serializable {
+
+    private Integer id;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ helloworld/src/test/java/kr/ac/jejunu/hibernate/BasicHibernateTest.java
@@ -0,0 +1,45 @@
+package kr.ac.jejunu.hibernate;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import kr.ac.jejunu.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by hyh0408 on 2016. 5. 19..
+ */
+public class BasicHibernateTest {
+
+    SessionFactory sessionFactory;
+
+    @Before
+    public void setup() {
+        Configuration configuration = new Configuration().configure("jejunu.cfg.xml");
+        configuration.addResource("User.hbm.xml");
+        StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+        sb.applySettings(configuration.getProperties());
+        StandardServiceRegistry standardServiceRegistry = sb.build();
+        sessionFactory = configuration.buildSessionFactory(standardServiceRegistry);
+    }
+
+    @After
+    public void finish() {
+        sessionFactory.close();
+    }
+
+    @Test
+    public void getUserTest() {
+        Session session = sessionFactory.openSession();
+        User user = session.get(User.class, 1);
+        assertThat(user.getName(), is("허윤호"));
+        assertThat(user.getPassword(), is("1234"));
+    }
+
+}
