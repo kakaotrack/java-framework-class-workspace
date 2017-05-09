d696466439c62e8d1f9e8b655e3fabe868d832aa
hulk.h <hulk.h@kakaocorp.com>
2017-05-09 18:38:20 +0900

    Listener

---
+++ web/src/main/java/kr/ac/jejunu/servlet/HelloContextListener.java
@@ -0,0 +1,25 @@
+package kr.ac.jejunu.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by hyh0408 on 2017. 5. 9..
+ */
+public class HelloContextListener implements ServletContextListener{
+
+    private static Logger logger = LoggerFactory.getLogger(HelloContextListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        logger.info("***************  context init *************");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        logger.info("***************  context destroy *************");
+    }
+}
