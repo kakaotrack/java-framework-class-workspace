e2814f5b87732c0923a1191955d2b67280609824
hulk.h <hulk.h@kakaocorp.com>
2017-05-24 21:25:34 +0900

    hibernate one to many

---
+++ web/src/main/java/kr/ac/jejunu/model/Comment.java
@@ -0,0 +1,31 @@
+package kr.ac.jejunu.model;
+
+public class Comment {
+    private Integer id;
+    private User user;
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- web/src/main/java/kr/ac/jejunu/model/User.java
+++ web/src/main/java/kr/ac/jejunu/model/User.java
@@ -1,12 +1,15 @@
 package kr.ac.jejunu.model;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
 
 public class User implements Serializable {
 
     private Integer id;
     private String name;
     private String password;
+    private List<Comment> comments;
 
     public Integer getId() {
         return id;
@@ -30,5 +33,13 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
     }
 }
