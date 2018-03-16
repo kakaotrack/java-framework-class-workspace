7e12ce1950fb3e2e65c34d3eb2899d6993845cfc
hulk.h <hulk.h@kakaocorp.com>
2018-03-16 17:32:49 +0900

    extract method

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -4,28 +4,47 @@ import java.sql.*;
 
 public class UserDao {
     public User get(int id) throws ClassNotFoundException, SQLException {
-        //mysql driver load
-        Class.forName("com.mysql.jdbc.Driver");
-        //Connection 맺고
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju"
-                , "jeju", "jejupw");
-        //sql 작성하고
+        Connection connection = getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setInt(1, id);
-        //sql 실행하고
         ResultSet resultSet = preparedStatement.executeQuery();
-        //결과를 User 에 매핑하고
         resultSet.next();
         User user = new User();
         user.setId(resultSet.getInt("id"));
         user.setName(resultSet.getString("name"));
         user.setPassword(resultSet.getString("password"));
-        //자원을 해지하고
         resultSet.close();
         preparedStatement.close();
         connection.close();
-        //결과를 리턴한다.
         return user;
+    }
+
+    public Integer insert(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "insert into userinfo(name, password) values (?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Integer id = resultSet.getInt(1);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?characterEncoding=utf-8"
+                , "jeju", "jejupw");
     }
 }
--- userdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ userdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -2,19 +2,42 @@ package kr.ac.jejunu;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
 
 
 public class UserDaoTest {
+
+    private UserDao userDao;
+
+    @Before
+    public void setup() {
+       userDao = new UserDao();
+    }
+
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
         int id= 1;
         User user = userDao.get(id);
         assertThat(user.getId(), is(1));
         assertThat(user.getName(), is("허윤호"));
         assertThat(user.getPassword(), is("1234"));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        user.setName("헐크");
+        user.setPassword("1111");
+        Integer id = userDao.insert(user);
+
+        User insertedUser = userDao.get(id);
+        assertThat(insertedUser.getId(), is(id));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
 }
