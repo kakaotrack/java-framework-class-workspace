c2a49408089a31055e9f5c0d170ef1a8de3dcb09
hulk.h <hulk.h@kakaocorp.com>
2018-04-06 15:27:47 +0900

    Spring dao factory

--- userdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ userdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -13,12 +15,11 @@ public class UserDaoTest {
 
     private UserDao userDao;
     private DaoFactory daoFactory;
-//    private UserDao hallaUserDao;
 
     @Before
     public void setup() {
-        daoFactory = new DaoFactory();
-        userDao = daoFactory.getUserDao();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
     }
 
     @Test
@@ -43,25 +44,4 @@ public class UserDaoTest {
         assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
 
-//    @Test
-//    public void hallaGet() throws SQLException, ClassNotFoundException {
-//        int id= 1;
-//        User user = hallaUserDao.get(id);
-//        assertThat(user.getId(), is(1));
-//        assertThat(user.getName(), is("허윤호"));
-//        assertThat(user.getPassword(), is("1234"));
-//    }
-//
-//    @Test
-//    public void hallaAdd() throws SQLException, ClassNotFoundException {
-//        User user = new User();
-//        user.setName("헐크");
-//        user.setPassword("1111");
-//        Integer id = hallaUserDao.insert(user);
-//
-//        User insertedUser = hallaUserDao.get(id);
-//        assertThat(insertedUser.getId(), is(id));
-//        assertThat(insertedUser.getName(), is(user.getName()));
-//        assertThat(insertedUser.getPassword(), is(user.getPassword()));
-//    }
 }
