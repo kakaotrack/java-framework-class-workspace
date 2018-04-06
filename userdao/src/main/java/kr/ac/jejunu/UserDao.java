f2fc27afd39573d687df34c8a238869c01637019
hulk.h <hulk.h@kakaocorp.com>
2018-04-06 17:49:48 +0900

    delete, update datasource

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,29 +1,31 @@
 package kr.ac.jejunu;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public User get(int id) throws ClassNotFoundException, SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-        User user;
+        User user = null;
         try {
-            connection = connectionMaker.getConnection();
+            connection = dataSource.getConnection();
             preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
             preparedStatement.setInt(1, id);
             resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            user = new User();
-            user.setId(resultSet.getInt("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
+            if(resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
         } finally {
             if (resultSet != null)
                 try {
@@ -53,7 +55,7 @@ public class UserDao {
         ResultSet resultSet = null;
         Integer id;
         try {
-            connection = connectionMaker.getConnection();
+            connection = dataSource.getConnection();
             preparedStatement = connection.prepareStatement(
                     "insert into userinfo(name, password) values (?, ?)");
             preparedStatement.setString(1, user.getName());
@@ -88,5 +90,63 @@ public class UserDao {
 
         }
         return id;
+    }
+
+    public void update(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "update userinfo set name = ?, password = ? where id = ?");
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setInt(3, user.getId());
+
+            preparedStatement.executeUpdate();
+
+        } finally {
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+        }
+    }
+
+    public void delete(Integer id) throws SQLException, ClassNotFoundException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "delete from userinfo where id = ?");
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+
+        } finally {
+            if (preparedStatement != null)
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+        }
     }
 }
--- userdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
+++ userdao/src/test/java/kr/ac/jejunu/UserDaoTest.java
@@ -25,6 +25,42 @@ public class UserDaoTest {
     }
 
     @Test
+    public void update() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        Integer id = insertUserTest(user);
+
+        user.setId(id);
+        user.setName("허윤호");
+        user.setPassword("1234");
+        userDao.update(user);
+
+        User updatedUser = userDao.get(id);
+
+        assertThat(updatedUser.getId(), is(user.getId()));
+        assertThat(updatedUser.getName(), is(user.getName()));
+        assertThat(updatedUser.getPassword(), is(user.getPassword()));
+
+    }
+
+    private Integer insertUserTest(User user) throws SQLException, ClassNotFoundException {
+        user.setName("헐크");
+        user.setPassword("1111");
+        return userDao.insert(user);
+    }
+
+    @Test
+    public void delete() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        Integer id = insertUserTest(user);
+
+        userDao.delete(id);
+
+        User deletedUser = userDao.get(id);
+        assertThat(deletedUser, nullValue());
+
+    }
+
+    @Test
     public void get() throws SQLException, ClassNotFoundException {
         int id= 1;
         User user = userDao.get(id);
@@ -36,9 +72,7 @@ public class UserDaoTest {
     @Test
     public void add() throws SQLException, ClassNotFoundException {
         User user = new User();
-        user.setName("헐크");
-        user.setPassword("1111");
-        Integer id = userDao.insert(user);
+        Integer id = insertUserTest(user);
 
         User insertedUser = userDao.get(id);
         assertThat(insertedUser.getId(), is(id));
