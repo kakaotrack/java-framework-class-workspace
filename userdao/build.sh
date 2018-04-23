51a428a266e67aafe4b1e8fa7c91cb37c8fb60ad
hulk.h <hulk.h@kakaocorp.com>
2018-04-23 17:06:47 +0900

    build.sh

---
+++ userdao/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PROJECT_ROOT=/daum/jeju2018/java-framework-class-workspace/userdao
+SRC_PATH=$PROJECT_ROOT/src/main/java
+DEST_PATH=$PROJECT_ROOT/out/build
+LIB_PATH=$PROJECT_ROOT/lib
+RESOURCE_PATH=$PROJECT_ROOT/src/main/resources
+libs=''
+for lib in $(ls $LIB_PATH)
+do
+ 	lib=$LIB_PATH/$lib
+	libs="$libs$lib:"
+done
+
+echo 'clean : start'
+rm -rf $DEST_PATH
+mkdir -p $DEST_PATH
+echo 'clean : end'
+echo 'compile : start'
+javac -cp $libs -d $DEST_PATH $(find $SRC_PATH -name "*.java")
+echo 'compile : end'
+cp -r $RESOURCE_PATH/* $DEST_PATH
+echo 'buid : start'
+jar cvf $DEST_PATH/userdao.jar $(find $DEST_PATH -name "*.class" -o -name "*.xml")
+echo find $DEST_PATH -name "*.class *.xml"
+echo 'buid : end'
