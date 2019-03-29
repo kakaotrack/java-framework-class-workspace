9092c908be373ad009270ed0640e44f6660dd4ca
hulk.h <hulk.h@kakaocorp.com>
2019-03-29 16:29:14 +0900

    factory method pattern

--- userdao/src/kr/ac/jejunu/userdao/UserDaoTests.java
+++ userdao/src/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -14,7 +14,7 @@ public class UserDaoTests {
         Long id = 1l;
         String name = "허윤호";
         String password = "1234";
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         User user = userDao.get(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -28,13 +28,26 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         Long id = userDao.add(user);
         User resultUser = userDao.get(id);
         assertThat(resultUser.getId(), is(id));
         assertThat(resultUser.getName(), is(name));
         assertThat(resultUser.getPassword(), is(password));
     }
+
+    @Test
+    public void testHallaGet() throws SQLException, ClassNotFoundException {
+        Long id = 1l;
+        String name = "헐크";
+        String password = "1111";
+        UserDao userDao = new HallaUserDao();
+        User user = userDao.get(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
 }
 
 
