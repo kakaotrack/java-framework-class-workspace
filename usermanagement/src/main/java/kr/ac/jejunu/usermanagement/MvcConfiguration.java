61a4a9eb6212a0c04c826d421ad29e90a3d2823e
hulk.h <hulk.h@kakaocorp.com>
2020-04-03 14:21:32 +0900

    usermanagement

---
+++ usermanagement/src/main/java/kr/ac/jejunu/usermanagement/MvcConfiguration.java
@@ -0,0 +1,15 @@
+package kr.ac.jejunu.usermanagement;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/list");
+        registry.addViewController("/edit");
+        registry.addViewController("/create").setViewName("edit");
+    }
+}
---
+++ usermanagement/src/main/java/kr/ac/jejunu/usermanagement/User.java
@@ -0,0 +1,24 @@
+package kr.ac.jejunu.usermanagement;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "userinfo")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String password;
+}
