4047e317ab46ff5d0aa527b1a44e1ef89f54b4cd
hulk.h <hulk.h@kakaocorp.com>
2021-03-19 18:28:08 +0900

    no message

--- userdao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -2,6 +2,8 @@ package kr.ac.jejunu;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDao {
     public User findById(Integer id) {
@@ -13,5 +15,16 @@ public class UserDao {
                                 "characterEncoding=utf-8&serverTimezone=UTC"
                         ,"jeju","jejupw"
                 );
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "select * from  userinfo where id = ?"
+        );
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
     }
 }
+
+
+
+
+
+
