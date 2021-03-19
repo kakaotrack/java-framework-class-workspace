3845c0c637c57a57df1e2f0f92fd141d2fc8193b
hulk.h <hulk.h@kakaocorp.com>
2021-03-19 18:26:04 +0900

    no message

--- userdao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,8 +1,17 @@
 package kr.ac.jejunu;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class UserDao {
     public User findById(Integer id) {
         //데이터 어딨어? => mysql
-
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection(
+                        "jdbc:mysql://localhost/jeju?" +
+                                "characterEncoding=utf-8&serverTimezone=UTC"
+                        ,"jeju","jejupw"
+                );
     }
 }
