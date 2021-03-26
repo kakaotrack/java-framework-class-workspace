b0852ac5d013d6dd78698ab79a4e3d5a31279c39
hulk.h <hulk.h@kakaocorp.com>
2021-03-26 16:29:35 +0900

    insert

--- userdao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -27,6 +27,44 @@ public class UserDao {
         connection.close();
         return user;
     }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        //데이터 어딨어? => mysql
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection =
+                DriverManager.getConnection(
+                        "jdbc:mysql://localhost/jeju?" +
+                                "characterEncoding=utf-8&serverTimezone=UTC"
+                        ,"jeju","jejupw"
+                );
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into userinfo (name, password) values ( ?, ? )"
+                , Statement.RETURN_GENERATED_KEYS
+        );
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+
+        user.setId(resultSet.getInt(1));
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
--- userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
@@ -20,4 +21,41 @@ public class UserDaoTests {
         assertThat(user.getName(), is(name));
         assertThat(user.getPassword(), is(password));
     }
+
+    @Test
+    public void insert() throws SQLException, ClassNotFoundException {
+        String name = "허윤호";
+        String password = "1111";
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        userDao.insert(user);
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(user.getId(), greaterThan(0));
+        assertThat(insertedUser.getId(), is(user.getId()));
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
