f2fad88fbb107e080c0c8cafbdb27473af871926
hulk.h <hulk.h@kakaocorp.com>
2021-04-02 17:39:44 +0900

    exception

--- userdao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -12,42 +12,74 @@ public class UserDao {
 
     public User findById(Integer id) throws SQLException {
         //데이터 어딨어? => mysql
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "select * from  userinfo where id = ?"
-        );
-        preparedStatement.setInt(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        User user = new User();
-        user.setId(resultSet.getInt("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "select * from  userinfo where id = ?"
+            );
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            user = new User();
+            user.setId(resultSet.getInt("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+        }
         return user;
     }
 
     public void insert(User user) throws SQLException {
         //데이터 어딨어? => mysql
-        Connection connection = dataSource.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "insert into userinfo (name, password) values ( ?, ? )"
-                , Statement.RETURN_GENERATED_KEYS
-        );
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement(
+                    "insert into userinfo (name, password) values ( ?, ? )"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
 
-        preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
 
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
 
-        user.setId(resultSet.getInt(1));
+            user.setId(resultSet.getInt(1));
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+        }
 
-        preparedStatement.close();
-        connection.close();
     }
 
 }
