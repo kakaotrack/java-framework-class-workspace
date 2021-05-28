00e53ba53a8274d5aaa85c4d1530433ecfbd2375
hulk.h <hulk.h@kakaocorp.com>
2021-05-28 17:56:43 +0900

    file upload

--- userdao2/src/main/java/kr/ac/jejunu/UserController.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserController.java
@@ -1,9 +1,17 @@
 package kr.ac.jejunu;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
 
 @Controller
 @RequestMapping
@@ -14,5 +22,30 @@ public class UserController {
     @RequestMapping("/user")
     public User getUser(@RequestParam("id") Integer id) {
         return userDao.findById(id);
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public void upload() {
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        String path = request.getServletContext().getRealPath("/")
+                + "/WEB-INF/static/" + file.getOriginalFilename();
+        File saveFile = new File(path);
+        FileOutputStream fileOutputStream = new FileOutputStream(saveFile);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url", "/images/" + file.getOriginalFilename());
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("e", e);
+        return modelAndView;
     }
 }
---
+++ userdao2/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,6 @@
+<%@page contentType="text/html; charset=UTF-8" %>
+<html>
+<body>
+<h2>Error  ${e.message}</h2>
+</body>
+</html>
