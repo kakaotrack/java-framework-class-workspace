71c2d3090b03059f262269cbba88457a5be17b78
hulk.h <hulk.h@kakaocorp.com>
2022-03-18 18:30:28 +0900

    userdao2

--- userdao2/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,8 +1,36 @@
 package kr.ac.jejunu;
 
+import java.sql.*;
+
 public class UserDao {
-    public User findById(Integer id) {
-        //
-        return null;
+    public User findById(Integer id) throws ClassNotFoundException, SQLException {
+        //드라이버 로딩
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //커넥션
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://192.168.151.176:3306/jeju?serverTimezone=UTC",
+                "jeju",
+                "jejupw"
+        );
+        //sql 작성
+        PreparedStatement preparedStatement =
+                connection.prepareStatement(
+                        "select * from userinfo where id = ?"
+                );
+        preparedStatement.setInt(1, id);
+        //sql 실행
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        //User 에 데이터 매핑
+        User user = new User();
+        user.setId(resultSet.getInt("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+        //자원 해지
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //User 리턴
+        return user;
     }
 }
--- userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
+++ userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -3,12 +3,14 @@ package kr.ac.jejunu;
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class UserDaoTests {
     @Test
-    public void findById() {
+    public void findById() throws SQLException, ClassNotFoundException {
         Integer id = 1;
         String name = "hulk";
         String password = "1234";
