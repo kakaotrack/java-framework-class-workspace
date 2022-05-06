b269f6db7c18a8ea3fdc7d2a4e8a5ca0c67a2765
hulk.h <hulk.h@kakaocorp.com>
2022-05-06 17:02:12 +0900

    spring mvc 1

--- userdao2/src/main/java/kr/ac/jejunu/UserServlet.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserServlet.java
@@ -1,5 +1,9 @@
 package kr.ac.jejunu;
 
+import lombok.SneakyThrows;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -7,6 +11,7 @@ import javax.servlet.ServletResponse;
 import java.io.IOException;
 
 public class UserServlet extends GenericServlet {
+    private UserDao userDao;
     @Override
     public void destroy() {
         System.out.println("********* destroy ***********");
@@ -15,14 +20,20 @@ public class UserServlet extends GenericServlet {
 
     @Override
     public void init() throws ServletException {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext("kr.ac.jejunu");
+        this.userDao = applicationContext.getBean("userDao", UserDao.class);
         System.out.println("*********  init ***********");
         super.init();
     }
 
+    @SneakyThrows
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        Integer id = Integer.parseInt(req.getParameter("id"));
+        User user = userDao.findById(id);
         res.setContentType("text/html;charset=UTF-8");
-        String html = "<html><h1>Hi 허윤호</h1></html>";
+        String html = String.format("<html><h1>Hi %s</h1></html>", user.getName());
         res.getWriter().println(html);
         System.out.println("********* service ***********");
     }
---
+++ userdao2/src/main/webapp/WEB-INF/views/user.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<body>
+<h2>Hi JSP id: ${user.id}  name: ${user.name}</h2>
+</body>
+</html>
