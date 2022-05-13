2d7e31e8d0b217ebf59790d665d6de75a2c7286f
hulk.h <hulk.h@kakaocorp.com>
2022-05-13 18:32:12 +0900

    no message

--- userdao2/src/main/java/kr/ac/jejunu/UserController.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserController.java
@@ -1,22 +1,24 @@
 package kr.ac.jejunu;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.*;
+import java.util.Date;
 
 @Controller
 public class UserController {
-    @RequestMapping("/user")
+    @GetMapping(path = "/user/{id}/{name}", produces = "application/json")
     public ModelAndView user(
-            @RequestParam("id") Integer id
-            , @RequestParam("name") String name
+            @PathVariable("id") Integer id
+            , @PathVariable("name") String name
     ) {
         User user = new User();
         user.setId(id);
@@ -26,12 +28,22 @@ public class UserController {
         return modelAndView;
     }
 
-    @RequestMapping("/upload")
+    @GetMapping("/upload")
     public void upload() {
 
     }
 
-    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    @RequestMapping("/date")
+    public ModelAndView date(
+            @RequestParam(required = false, defaultValue = "2022-03-03")
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                    Date date) {
+        ModelAndView modelAndView = new ModelAndView("date");
+        modelAndView.addObject("date", date);
+        return modelAndView;
+    }
+
+    @PostMapping("/upload")
     public ModelAndView upload(@RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
         File path = new File(request.getServletContext()
                 .getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
@@ -43,6 +55,23 @@ public class UserController {
         modelAndView.addObject("url"
                 , "/images/" + file.getOriginalFilename());
         return modelAndView;
+    }
+
+    @GetMapping(path = "cookie", produces = "application/json")
+    public Cookie[] cookie(@CookieValue("hulk") String hulk, HttpServletRequest request, HttpServletResponse response) {
+
+        Cookie cookie = new Cookie("hulk", "1234");
+        response.addCookie(cookie);
+        return request.getCookies();
+    }
+
+    @GetMapping(path = "session", produces = "application/json")
+    public User session(HttpSession httpSession) {
+        User user = (User) httpSession.getAttribute("user");
+        if(user == null)
+            httpSession.setAttribute("user"
+                    , User.builder().name("session").password("1111").build());
+        return user;
     }
 
     @ExceptionHandler(Exception.class)
---
+++ userdao2/src/main/webapp/WEB-INF/views/date.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<body>
+<h2>date : ${date}</h2>
+</body>
+</html>
