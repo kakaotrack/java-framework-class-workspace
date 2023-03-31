87944c8169197e46837265ca731d6edcad056aa3
hulk.h <hulk.h@kakaocorp.com>
2023-03-31 17:34:25 +0900

    datasource

--- userdao2/src/main/java/kr/ac/jejunu/user/DaoFactory.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/DaoFactory.java
@@ -1,18 +1,37 @@
 package kr.ac.jejunu.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+    @Value("${db.classname}")
+    private String className;
+    @Value("${db.url}")
+    private String url;
+
     @Bean
-    public UserDao userDao() {
-        UserDao userDao = new UserDao(connectionMaker());
+    public UserDao userDao() throws ClassNotFoundException {
+        UserDao userDao = new UserDao(dataSource());
         return userDao;
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new JejuConnectionMaker();
+    public DataSource dataSource() throws ClassNotFoundException {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass((Class<? extends Driver>) Class.forName(className));
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 }
--- userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -1,18 +1,19 @@
 package kr.ac.jejunu.user;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     public User findById(Long id) throws ClassNotFoundException, SQLException {
         //데이터 어딨어? mysql
         //mysql 클래스 로딩
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         //쿼리 만들고
         PreparedStatement preparedStatement = connection.prepareStatement
                 ("select id, name, password from userinfo where id = ?");
@@ -35,7 +36,7 @@ public class UserDao {
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
+        Connection connection = dataSource.getConnection();
         //쿼리 만들고
         PreparedStatement preparedStatement = connection.prepareStatement
                 ("insert into userinfo (name, password) values ( ?, ? )"
