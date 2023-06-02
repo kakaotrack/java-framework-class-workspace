404f4448b30e7b9c828b23594c669a2c02e2db34
hulk.h <hulk.h@kakaocorp.com>
2023-06-02 17:47:58 +0900

    no message

---
+++ user/src/main/java/kr/ac/jejunu/user/WebConfig.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.user;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
